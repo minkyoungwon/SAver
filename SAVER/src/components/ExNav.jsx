c6740aa291b56715f6945f81f2118c916d00c401
Lim J <heuristic2022@gmail.com>
2024-12-17 15:02:20 +0900

    약간 수정정

---
+++ SAVER/src/components/ExNav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Ex2() {
+  return (
+    <nav>
+        <a href="index.html">메인</a>
+        <a href="aboutMe.html">소개</a>
+        <a href="ability.html">기술</a>
+        <a href="contact.html">연락처</a>
+    </nav>
+  )
+}
+
+export default Ex2
---
+++ SAVER/src/components/Example.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function Example() {
+  return (
+    <div>
+        
+    <header>
+        <h1>끊임없이 성장하는 개발자</h1>
+    </header>
+
+    <figure class="my-card">
+
+        <div class="intro-box">
+            <h3>안녕하세요. <span>저희는는</span></h3>
+            <h1>감 다 팀</h1>
+            <h3>입니다.</h3>
+        </div>
+    </figure>
+    <main>
+        <section class="intro-container">
+            <article class="intro-card">
+                <a href="aboutMe.html">
+                    <h3>소개</h3>
+                    <p>열정, 성장, 협동</p>
+                </a>
+            </article>
+            <article class="intro-card">
+                <a href="ability.html">
+                    <h3>기술</h3>
+                    <p>개발 언어와 다양한 개발 지식을 배우고 있습니다.</p>
+                </a>
+            </article>
+            <article class="intro-card">
+                <a href="contact.html">
+                    <h3>연락처</h3>
+                    <p>많은 연락 바랍니다.</p>
+                </a>
+            </article>
+        </section>
+    </main>
+    <footer>
+        <p>&copy; 2024 새싹. All rights reserved</p>
+    </footer>
+    </div>
+  )
+}
+
+export default Example
