c0635070dd5dbfc9c595019757d12ff9eb86ec9c
Lim J <heuristic2022@gmail.com>
2024-12-30 11:37:35 +0900

    [add]카테고리 추가 기능

--- CLIENT/src/components/CouponCategory.jsx
+++ CLIENT/src/components/CouponCategory.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
+const CouponCategory = ({category, addCategory, handleCategoryClick}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [input, setInput] = useState("");
+  const openModal = () => {
+    setIsModalOpen(true);
+  }
+  const closeModal = () => {
+    setIsModalOpen(false);
+  }
 
-const CouponCategory = ({category, handleCategoryClick}) => {
 
   return (
-    <div>
-        <button onClick={() => handleCategoryClick("all")} className="rounded-full bg-gray-100 hover:bg-gray-200 h-7 w-21 text-gray-600">+</button>
+    <>
+    <div className='flex justify-center items-center gap-4' >
+        <button onClick={() => openModal()} className="rounded-full bg-gray-100 hover:bg-gray-200 w-20 h-7text-gray-600">+</button>
         {category.map((item) => (
-            <button onClick={handleCategoryClick(item)} className="rounded-full bg-gray-100 hover:bg-gray-200 h-7 w-21 text-gray-600">{item}</button>
+            <button onClick={() => handleCategoryClick(item)} className="rounded-full bg-gray-100 hover:bg-gray-200  w-20 text-gray-600">{item}</button>
         ))}
     </div>
+    {isModalOpen && (
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="bg-white p-6 rounded-lg">
+          <input type="text" placeholder="카테고리 추가" value={input} onChange={(e) => setInput(e.target.value)} />
+          <div className='flex justify-end gap-4'>
+          <button onClick={() => addCategory(input)} className='bg-gray-100 hover:bg-gray-200 w-20 h-7 text-gray-600'>추가</button>
+          <button onClick={closeModal} className='bg-gray-100 hover:bg-gray-200 w-20 h-7 text-gray-600'>닫기</button>
+          </div>
+        </div>
+      </div>
+    )}
+    </>
   )
 }
 
