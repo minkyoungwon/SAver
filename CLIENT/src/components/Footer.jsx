d5a3891d56360a7fcfce172734975cd5f270bfd3
hyemi11 <ham.c113@gmail.com>
2025-01-15 14:30:41 +0900

    [update] 네브바 메뉴탭 상태관리 추가, 푸터 진행중

--- CLIENT/src/components/Footer.jsx
+++ CLIENT/src/components/Footer.jsx
@@ -1,12 +1,27 @@
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+  const location = useLocation();
+
   return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-8">
+    <footer className=" bg-gray-800 text-white text-center py-4 mt-8">
       <p>&copy; {new Date().getFullYear()} My Board App. All rights reserved.</p>
-	  <h4> 임요한 <a href='https://github.com/LimJ2023'> Github </a></h4>
-	  <h4> 이도윤 <a href='https://github.com/dohyounLee'> giThub </a></h4>
-	  <h4> 최혜미 <a href='https://github.com/hyemi11'> gitHub </a></h4>
-	  <h4> 민경원 <a href='https://github.com/minkyoungwon'> GithUB </a></h4>
+      <h4>
+        {" "}
+        임요한 <a href="https://github.com/LimJ2023"> Github </a>
+      </h4>
+      <h4>
+        {" "}
+        이도윤 <a href="https://github.com/dohyounLee"> giThub </a>
+      </h4>
+      <h4>
+        {" "}
+        최혜미 <a href="https://github.com/hyemi11"> gitHub </a>
+      </h4>
+      <h4>
+        {" "}
+        민경원 <a href="https://github.com/minkyoungwon"> GithUB </a>
+      </h4>
     </footer>
   );
 };
--- CLIENT/src/components/Header.jsx
+++ CLIENT/src/components/Header.jsx
@@ -1,22 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "/src/assets/logo.png";
 import logo2 from "/src/assets/logo2.svg";
 import logo3 from "/src/assets/logo3.svg";
 
 const Header = ({ user, handleLogout }) => {
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
+
   return (
-    <div className="content-wrapper sticky top-0 z-10 ">
+    <div className=" sticky top-0 z-10 ">
       <header>
-        <div className="py-4 w-full flex justify-between items-center bg-white">
+        <div className="py-4 px-[16px] md:px-[15%] text-lg w-full flex justify-between items-center text-emerald-600  bg-white">
           <nav className="flex justify-center items-center">
             <Link to="/">
               <img src={logo3} alt="logo" className=" mr-6 w-30 h-8 pb-1 rounded-md" />
             </Link>
             <Link to="/">
-              <div className=" pr-6 hover: ">쿠폰</div>
+              <div className={` pr-6 hover:font-medium ${isActive("/") ? "font-medium" : ""}`}>쿠폰</div>
             </Link>
+
             <Link to="/board">
-              <div className=" pr-6 hover: ">게시판</div>
+              <div className={` pr-6 hover:font-medium ${isActive("/board") ? "font-medium " : ""}`}>게시판</div>
             </Link>
           </nav>
           <nav className="flex justify-end space-x-4">
@@ -33,7 +37,7 @@ const Header = ({ user, handleLogout }) => {
                 </button>
               </>
             ) : (
-              <Link to="/login" className="text-sm mr-4">
+              <Link to="/login" className="text-sm  ">
                 로그인
               </Link>
             )}
