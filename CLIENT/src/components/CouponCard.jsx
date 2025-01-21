972000c067aaf6b609d43bf7bef78cf4c297271f
hyemi11 <ham.c113@gmail.com>
2025-01-21 15:19:28 +0900

    [update] 1.deadline 포맷팅 1. 남은일자 뱃지 표시(일자가 남은 쿠폰만 뱃지 랜더링함)

--- CLIENT/src/components/CouponCard.jsx
+++ CLIENT/src/components/CouponCard.jsx
@@ -22,6 +22,20 @@ const CouponCard = ({ coupon }) => {
     closeModal();
   };
 
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    date.setDate(date.getDate() + 1); // 날짜를 1일 증가시킴 (coupon.deadline이 실제 날자보다 1일 적어서 임의로 +1시킴)
+    return date.toISOString().split('T')[0]; // 'YYYY-MM-DD' 포맷 반환
+  };
+
+  const getRemainingDays = (deadline) => {
+    const today = new Date();
+    const targetDate = new Date(deadline);
+    const diffTime = targetDate - today; // 차이 (밀리초)
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // 밀리초 → 일수
+    return diffDays;
+  };
+
   return (
     <>
       <div className="grid grid-cols-[150px_auto]  md:grid-cols-[150px_auto_80px] gap-6 p-6 border rounded-md  bg-white cursor-pointer relative hover:shadow-md " onClick={floatDetailModal}>
@@ -32,7 +46,10 @@ const CouponCard = ({ coupon }) => {
         <div className="flex items-top pt-1 justify-start bg-white">
           <div>
             <div className="flex space-x-3 pb-3">
-              <span className="badge">{"7일남음"}</span>
+              {/* 남은 일수가 양수일 때만 뱃지를 렌더링(0일 포함, -1부터 뱃지 없음)*/}
+              {getRemainingDays(coupon.deadline) >= 0 && (
+                <span className="badge">{getRemainingDays(coupon.deadline)}일 남음</span>
+              )}
               {isUsed && <span className="badge bg-emerald-400 text-neutral-50">사용완료</span>}
               {isExpired && <span className="badge bg-neutral-200 text-gray-600">기간만료</span>}
               {!isUsed && !isExpired && <span className="badge bg-yellow-300 text-gray-600">사용가능</span>}
@@ -44,13 +61,13 @@ const CouponCard = ({ coupon }) => {
                 <p className="text-sm text-gray-600">{coupon.usage_location}</p>
               </div>
             </div>
-            <p className="absolute bottom-6 text-sm text-gray-500 md:hidden"> {coupon.deadline} 까지 </p>
+            <p className="absolute bottom-6 text-sm text-gray-500 md:hidden"> {formatDate(coupon.deadline)} 까지 </p>
           </div>
         </div>
 
         {/* 세 번째 (오른쪽 정렬) */}
         <div className="hidden md:flex items-center justify-end bg-white">
-          <p className="text-sm text-gray-500"> {coupon.deadline} 까지</p>
+          <p className="text-sm text-gray-500"> {formatDate(coupon.deadline)} 까지</p>
         </div>
 
         {isModalOpen && (
